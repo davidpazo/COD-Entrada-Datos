6eb19fdb76f24055257e107a6a669d4136ddcb6a
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2015-02-19 09:01:18 +0100

    Commit inicial

---
+++ src/cod/EntradaDatos.java
@@ -0,0 +1,33 @@
+package cod;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author dpazolopez
+ */
+public class EntradaDatos {
+/**
+ * Introducir numero
+ * @param num; SI num=0 introduce por pantalla, si num=1 introduce por dialogo.
+ * @return op
+ */
+    public static int introducir(int num) {
+        Scanner dato = new Scanner(System.in);
+        int op = 0;
+
+        switch (num) {
+            case 0:
+                op = dato.nextInt();
+                System.out.println(op);
+            case 1:
+                op = Integer.parseInt(JOptionPane.showInputDialog("Introduce dato"));
+                JOptionPane.showMessageDialog(null, op);
+            default:
+                op = 0;
+        }
+        return op;
+
+    }
+
+}
